51ed0d2ead94e42313589a6e4e52c4b950da9e04
education-bootcamp <education.seekerscloud@gmail.com>
2026-06-24 21:32:17 +0530

    init

--- src/app/services/auth/auth-service.ts
+++ src/app/services/auth/auth-service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RegisterRequestDto } from '../../dto/RegisterRequestDto';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   httpClient = inject(HttpClient);
-
-  private authUrl='http://localhost:8081/auth/register';
+  
+  private authUrl=`${environment.BASEURL}/auth/register`;
 
   public createUser(data: RegisterRequestDto):Observable<any>{
      return this.httpClient.post(this.authUrl,data);
